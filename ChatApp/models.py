aa5a2318066c2f0d4f831e2cd7048e0d09d45d3f
yasuonagaro <yasuo.nagaro@gmail.com>
2025-10-24 02:36:08 +0900

    グループ一覧ページを表示する関数を追加。合わせて、Groupクラスを作成し、グループデータを取得するクラスメソッドを追加

---
+++ ChatApp/models.py
@@ -0,0 +1,24 @@
+from flask import abort
+import pymysql
+from util.DB import DB
+
+# 初期起動時にコネクションプールを作成し接続を確立
+db_pool = DB.init_db_pool()
+
+
+# グループクラス
+class Group:
+    @classmethod
+    def get_all(cls):
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = "SELECT * FROM groups;"
+                cur.execute(sql)
+                groups = cur.fetchall()
+                return groups
+        except pymysql.Error as e:
+            print(f"エラーが発生しています：{e}")
+            abort(500)
+        finally:
+            db_pool.release(conn)
