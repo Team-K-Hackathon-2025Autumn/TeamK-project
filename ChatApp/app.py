289ca539b9803b6cdb37a1b44c8ba7984cd8e332
yasuonagaro <yasuo.nagaro@gmail.com>
2025-10-24 02:11:21 +0900

    ログインページを表示する関数を追加

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -41,3 +41,14 @@ def index_process():
     if uid is None:
         return redirect(url_for("login_view"))
     return redirect(url_for("home_view"))
+
+
+# ログインページ表示
+@app.route("/login", methods=["GET"])
+def login_view():
+    uid = session.get("uid")
+    if uid is None:
+        return render_template("auth/login.html")
+    return redirect(
+        url_for("home_view")
+    )  # ログイン済みの場合、グループ一覧にリダイレクト
