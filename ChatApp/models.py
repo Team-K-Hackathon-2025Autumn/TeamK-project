387820f0762bb0f3ed1c47b5f8d67198508a44c9
yasuonagaro <yasuo.nagaro@gmail.com>
2025-10-26 01:15:29 +0900

    関数コメントを修正

--- ChatApp/models.py
+++ ChatApp/models.py
@@ -9,52 +9,6 @@ db_pool = DB.init_db_pool()
 # グループクラス
 class Group:
     @classmethod
-    def get_all(cls):
-        conn = db_pool.get_conn()
-        try:
-            with conn.cursor() as cur:
-                sql = "SELECT * FROM groups;"
-                cur.execute(sql)
-                groups = cur.fetchall()
-                return groups
-        except pymysql.Error as e:
-            print(f"エラーが発生しています：{e}")
-            abort(500)
-        finally:
-            db_pool.release(conn)
-
-    @classmethod
-    def find_by_gid(cls, gid):
-        conn = db_pool.get_conn()
-        try:
-            with conn.cursor() as cur:
-                sql = "SELECT * FROM groups WHERE gid=%s;"
-                cur.execute(sql, (gid,))
-                groups = cur.fetchone()
-                return groups
-        except pymysql.Error as e:
-            print(f"エラーが発生しています：{e}")
-            abort(500)
-        finally:
-            db_pool.release(conn)
-
-    @classmethod
-    def update(cls, gid, new_group_name):
-        conn = db_pool.get_conn()
-        try:
-            with conn.cursor() as cur:
-                sql = "UPDATE groups SET name=%s, WHERE id=%s;"
-                cur.execute(sql, (gid, new_group_name))
-                conn.commit()
-                groups = cur.fetchall()
-                return groups
-        except pymysql.Error as e:
-            print(f"エラーが発生しています：{e}")
-            abort(500)
-        finally:
-            db_pool.release(conn)
-
-    @classmethod
     def delete(cls, gid):
         conn = db_pool.get_conn()
         try:
