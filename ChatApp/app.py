764ac425ded05876972e239ba209c24f9fb43f51
yasuonagaro <yasuo.nagaro@gmail.com>
2025-10-25 23:47:58 +0900

    メッセージ一覧表示の関数を追加。合わせてメッセージクラスを追加し、グループIDをキーにメッセージを取得するクラスメソッドを追加

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -96,3 +96,17 @@ def delete_group(gid):
             Group.delete(gid)
 
         return redirect("home_view")
+
+
+# メッセージ一覧ページ表示（各グループ内で、そのグループに属している全メッセージを表示させる）
+@app.route("/group/<gid>", methods=["POST"])
+def message_view(gid):
+    uid = session.get("uid")
+    if uid is None:
+        return redirect(url_for("login_view"))
+    else:
+        group = Group.find_by_gid(gid)
+        messages = Message.get_all(gid)
+        return render_template(
+            "group/messages.html", messages=messages, group=group, uid=uid
+        )
--- ChatApp/models.py
+++ ChatApp/models.py
@@ -67,3 +67,28 @@ class Group:
             abort(500)
         finally:
             db_pool.release(conn)
+
+
+# メッセージクラス
+class Message:
+    @classmethod
+    def get_all(cls, gid):
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = """
+                    SELECT m.id, m.uid, u.name, m.message, r.counts, m.created_at 
+                    FROM messages AS m 
+                    INNER JOIN users AS u ON m.uid = u.id
+                    INNER JOIN eat_reactions AS r ON m.id = r.message_id 
+                    WHERE m.gid = %s 
+                    ORDER BY m.id ASC;
+                """
+                cur.execute(sql, (gid,))
+                messages = cur.fetchall()
+                return messages
+        except pymysql.Error as e:
+            print(f"エラーが発生しています：{e}")
+            abort(500)
+        finally:
+            db_pool.release(conn)
