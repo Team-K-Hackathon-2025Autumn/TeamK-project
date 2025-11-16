e74828c31123a2c1a5c41c3205d179078564c752
yasuonagaro <yasuo.nagaro@gmail.com>
2025-11-16 14:41:03 +0900

    リアクション時にリアクションしたメッセージ位置で画面再読み込みをするよう改善

--- ChatApp/static/js/scroll-message.js
+++ ChatApp/static/js/scroll-message.js
@@ -1,6 +1,28 @@
 /*
-メッセージ一覧ページ内、ページ読み込み時に自動で下までスクロールする
+リアクションボタンを押した場合、スクロール位置をセッションストレージに保存。
+メッセージ一覧読み込み時に、セッションストレージに値があるかチェックし、存在した場合スクロール位置を使用して復元、復元後にセッションストレージを削除。
+値がない場合、最下部へスクロールした状態で表示
 */
 
 const element = document.getElementById('message-area');
-element.scrollTo(0, element.scrollHeight);
+const deleteButton = document.getElementById('button');
+const path = window.location.pathname;
+const savedscrollTopPosition = sessionStorage.getItem('scrollY_' + path);
+if (savedscrollTopPosition) {
+  element.scrollTop = parseInt(savedscrollTopPosition, 10);
+} else {
+  element.scrollTo(0, element.scrollHeight);
+}
+
+sessionStorage.removeItem('scrollY_' + path);
+
+const reactionButtons = document.querySelectorAll('.reaction-message-button');
+
+reactionButtons.forEach((button) => {
+  button.addEventListener('click', function (event) {
+    console.log('a');
+    const path = window.location.pathname;
+    const scrollTopPosition = element.scrollTop;
+    sessionStorage.setItem('scrollY_' + path, scrollTopPosition);
+  });
+});
