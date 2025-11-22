9894f59c257b47f9db8ef44d196647ead64dc9d9
yasuonagaro <yasuo.nagaro@gmail.com>
2025-11-22 19:24:43 +0900

    デザインを微調整

--- ChatApp/static/js/members/pagination.js
+++ ChatApp/static/js/members/pagination.js
@@ -9,7 +9,7 @@
 const pagination = () => {
   try {
     let page = 1; // 今何ページ目にいるか
-    const STEP = 8; // ステップ数（1ページに表示する項目数）
+    const STEP = 5; // ステップ数（1ページに表示する項目数）
 
     // 全ページ数を計算
     // 「メンバーの総数/(割る)ステップ数」の余りの有無で場合分け
@@ -19,28 +19,23 @@ const pagination = () => {
         ? members.length / STEP
         : Math.floor(members.length / STEP) + 1;
 
-    // ページネーションで表示されるページ数部分（< 1 2 3 >）の要素を作成
-    const paginationUl = document.querySelector(".pagination");
-    let pageCount = 0;
-    while (pageCount < TOTAL) {
-      let pageNumber = document.createElement("li");
-      pageNumber.dataset.pageNum = pageCount + 1;
-      pageNumber.innerText = pageCount + 1;
-      paginationUl.appendChild(pageNumber);
-      // ページネーションの数字部分が押された時にもページが変わるように処理
-      pageNumber.addEventListener("click", (e) => {
-        const targetPageNum = e.target.dataset.pageNum;
-        page = Number(targetPageNum);
-        init(page, STEP);
-      });
-      pageCount++;
-    }
+    // ページネーションで表示されるページ数部分（< 1 / 2 >）の要素を作成
+    const updatePageDisplay = (currentPage, totalPages) => {
+      const paginationUl = document.querySelector('.pagination');
+      // 中身をリセット
+      paginationUl.innerHTML = '';
+
+      const li = document.createElement('li');
+      li.innerText = `${currentPage} / ${totalPages}`;
+      li.style.pointerEvents = 'none';
+      paginationUl.appendChild(li);
+    };
 
     // メンバー名の要素を作成
     const createMembersList = (page, STEP) => {
-      const ul = document.querySelector(".member-box");
+      const ul = document.querySelector('.member-box');
       // 一度メンバーリストを空にする
-      ul.innerHTML = "";
+      ul.innerHTML = '';
 
       const firstMemberInPage = (page - 1) * STEP + 1;
       const lastMemberInPage = page * STEP;
@@ -48,41 +43,31 @@ const pagination = () => {
       // 各メンバー要素の作成
       members.forEach((member, i) => {
         if (i < firstMemberInPage - 1 || i > lastMemberInPage - 1) return;
-        const a = document.createElement("a");
-        const li = document.createElement("li");
+        const a = document.createElement('a');
+        const li = document.createElement('li');
         a.innerText = member.name;
         li.appendChild(a);
         ul.appendChild(li);
       });
     };
 
-    // ページネーション内で現在選択されているページの番号に色を付ける
-    const colorPaginationNum = () => {
-      // ページネーションの数字部分の全要素から"colored"クラスを一旦取り除く
-      const paginationArr = [...document.querySelectorAll(".pagination li")];
-      paginationArr.forEach((page) => {
-        page.classList.remove("colored");
-      });
-      // 選択されているページにclass="colored"を追加（文字色が変わる）
-      paginationArr[page - 1].classList.add("colored");
-    };
-
     const init = (page, STEP) => {
       createMembersList(page, STEP);
-      colorPaginationNum();
+      updatePageDisplay(page, TOTAL);
     };
+
     // 初期動作時に1ページ目を表示
     init(page, STEP);
 
     // 前ページ遷移
-    document.getElementById("prev").addEventListener("click", () => {
+    document.getElementById('prev').addEventListener('click', () => {
       if (page <= 1) return;
       page = page - 1;
       init(page, STEP);
     });
 
     // 次ページ遷移
-    document.getElementById("next").addEventListener("click", () => {
+    document.getElementById('next').addEventListener('click', () => {
       if (page >= members.length / STEP) return;
       page = page + 1;
       init(page, STEP);
@@ -97,7 +82,7 @@ const pagination = () => {
 
 pagination();
 // DOMツリーが構築されたらpagination関数を発火（ページネーションを作成し、その後チャンネル追加ボタンを作成・表示）
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
   try {
     pagination();
   } catch (error) {
--- ChatApp/static/js/time-reaction.js
+++ ChatApp/static/js/time-reaction.js
@@ -2,93 +2,103 @@
 const reactionCounts = document.querySelectorAll('.reaction-counts');
 
 // reactionCountsを一つずつelementとして処理
-reactionCounts.forEach(element => {
-    // elementの数を数値に10進数の数値に変換
-    const eachCount = parseInt(element.textContent,10);
-    // そのカウントが書かれているボタンをelementの親要素のbuttonとして取得
-    const reactionMsgBtn = element.closest('button');
-    reactionMsgBtn.style.transition = '0.3s ease';
-    // eachCountの値によってスタイルを変更
-    if (eachCount === 0) {
+reactionCounts.forEach((element) => {
+  // elementの数を数値に10進数の数値に変換
+  const eachCount = parseInt(element.textContent, 10);
+  // そのカウントが書かれているボタンをelementの親要素のbuttonとして取得
+  const reactionMsgBtn = element.closest('button');
+  // reactionMsgBtn.style.transition = '0.3s ease';
+  // eachCountの値によってスタイルを変更
+  if (eachCount === 0) {
     reactionMsgBtn.style.fontSize = '0.6rem';
     reactionMsgBtn.style.opacity = '0.3';
-} else if (eachCount < 5) {
+  } else if (eachCount < 5) {
     reactionMsgBtn.style.fontSize = '0.6rem';
     reactionMsgBtn.style.opacity = '1.0';
     // reactionMsgBtn.style.border = '1px #00c9ad solid';
     reactionMsgBtn.style.color = 'white';
-    reactionMsgBtn.style.backgroundColor = '#a97777'
-} else if (eachCount < 10) {
+    reactionMsgBtn.style.backgroundColor = '#a97777';
+  } else if (eachCount < 10) {
     reactionMsgBtn.style.fontSize = '0.8rem';
     reactionMsgBtn.style.opacity = '1.0';
     // reactionMsgBtn.style.border = '1px #00c9ad solid';
     reactionMsgBtn.style.color = 'white';
-    reactionMsgBtn.style.backgroundColor = '#a97777'
-} else if (eachCount < 20) {
+    reactionMsgBtn.style.backgroundColor = '#a97777';
+  } else if (eachCount < 20) {
     reactionMsgBtn.style.fontSize = '1.0rem';
     reactionMsgBtn.style.opacity = '1.0';
     reactionMsgBtn.style.color = 'white';
-    reactionMsgBtn.style.backgroundColor = '#a97777' 
+    reactionMsgBtn.style.backgroundColor = '#a97777';
     // reactionMsgBtn.style.border = '2px #00c9ad solid';
-} else if (eachCount < 100) {
+  } else if (eachCount < 100) {
     reactionMsgBtn.style.fontSize = '1.2rem';
     reactionMsgBtn.style.opacity = '1.0';
     reactionMsgBtn.style.color = 'white';
-    reactionMsgBtn.style.backgroundColor = '#a97777'
-    // reactionMsgBtn.style.border = '2px #00c9ad solid'; 
-} else {
+    reactionMsgBtn.style.backgroundColor = '#a97777';
+    // reactionMsgBtn.style.border = '2px #00c9ad solid';
+  } else {
     reactionMsgBtn.style.fontSize = '1.4rem';
     reactionMsgBtn.style.opacity = '1.0';
     reactionMsgBtn.style.color = 'white';
-    reactionMsgBtn.style.backgroundColor = '#a97777'
-    // reactionMsgBtn.style.border = '2px #00c9ad solid'; 
-}
+    reactionMsgBtn.style.backgroundColor = '#a97777';
+    // reactionMsgBtn.style.border = '2px #00c9ad solid';
+  }
 });
 
 // 画面に表示されたdatetimeを取得(他の人のコメント)
 const otherDateTimes = document.querySelectorAll('.other-sendTime');
 
 // dateTimesを一つずつ処理
-otherDateTimes.forEach(element2 => {
-    const eachDateTime = element2.textContent;
-    
-    // eachDateTimeをスペースの部分で分ける
-    const splitedDate = eachDateTime.split(' '),
+otherDateTimes.forEach((element2) => {
+  const eachDateTime = element2.textContent;
+
+  // eachDateTimeをスペースの部分で分ける
+  const splitedDate = eachDateTime.split(' '),
     // 分けた前半をymd、後半をhmsとして取得
-    ymd = splitedDate[0], hms =splitedDate[1];
+    ymd = splitedDate[0],
+    hms = splitedDate[1];
 
-    // さらにymdをハイフンの部分で分ける
-    const date = ymd.split('-'),
-    year = date[0], month = date[1], day = date[2];
+  // さらにymdをハイフンの部分で分ける
+  const date = ymd.split('-'),
+    year = date[0],
+    month = date[1],
+    day = date[2];
 
-    // さらにhmsをコロンの部分で分ける
-    const time = hms.split(':'),
-    hour = time[0], minute = time[1], second = time[2];
+  // さらにhmsをコロンの部分で分ける
+  const time = hms.split(':'),
+    hour = time[0],
+    minute = time[1],
+    second = time[2];
 
-    // element2に月日（改行）時分をセットする
-    element2.innerHTML = `${year}/${month}/${day}<br>${hour}:${minute}`
-})
+  // element2に月日（改行）時分をセットする
+  element2.innerHTML = `${year}/${month}/${day}<br>${hour}:${minute}`;
+});
 
 // 画面に表示されたdatetimeを取得(自分のコメント)
 const myDateTimes = document.querySelectorAll('.my-sendTime');
 
 // dateTimesを一つずつ処理
-myDateTimes.forEach(element2 => {
-    const eachDateTime = element2.textContent;
-    
-    // eachDateTimeをスペースの部分で分ける
-    const splitedDate = eachDateTime.split(' '),
+myDateTimes.forEach((element2) => {
+  const eachDateTime = element2.textContent;
+
+  // eachDateTimeをスペースの部分で分ける
+  const splitedDate = eachDateTime.split(' '),
     // 分けた前半をymd、後半をhmsとして取得
-    ymd = splitedDate[0], hms =splitedDate[1];
+    ymd = splitedDate[0],
+    hms = splitedDate[1];
 
-    // さらにymdをハイフンの部分で分ける
-    const date = ymd.split('-'),
-    year = date[0], month = date[1], day = date[2];
+  // さらにymdをハイフンの部分で分ける
+  const date = ymd.split('-'),
+    year = date[0],
+    month = date[1],
+    day = date[2];
 
-    // さらにhmsをコロンの部分で分ける
-    const time = hms.split(':'),
-    hour = time[0], minute = time[1], second = time[2];
+  // さらにhmsをコロンの部分で分ける
+  const time = hms.split(':'),
+    hour = time[0],
+    minute = time[1],
+    second = time[2];
 
-    // element2に月日（改行）時分をセットする
-    element2.innerHTML = `${year}/${month}/${day}<br>${hour}:${minute}`
-})
+  // element2に月日（改行）時分をセットする
+  element2.innerHTML = `${year}/${month}/${day}<br>${hour}:${minute}`;
+});
